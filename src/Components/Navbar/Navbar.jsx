e1f21de68e7651aa168b42d5db2c1a8d29cd4c35
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-09-25 20:17:30 +0600

    add navbar and banner section

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+import Image from "../../images/Logo.png"
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="flex justify-between items-center p-2"> 
+               <img src={Image} alt="image" /> 
+                <ul className="flex gap-5">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/donation"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                            }
+                        >
+                            Donation
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/statistics"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                            }
+                        >
+                           Statistics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,10 @@
+
+const Statistics = () => {
+    return (
+        <div>
+             This is Statistic
+        </div>
+    );
+};
+
+export default Statistics;
