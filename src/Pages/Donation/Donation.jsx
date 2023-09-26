00cf5db42467b67205da7bba07f281516fe0a67a
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-09-26 13:41:40 +0600

    add the functionality of show all button in donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,6 +4,7 @@ import DonationCard from "./DonationCard";
 const Donation = () => {
     const [donationsItem,setDonationsItem] =useState([]);
     const [noDonation,setNoDonation]=useState(false);
+    const [isShowAll,setIsShowAll]=useState(false);
 
     useEffect(()=>{
         const donationCard = JSON.parse(localStorage.getItem('card'));
@@ -28,14 +29,17 @@ const Donation = () => {
                 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mx-2">
                     {
-                        donationsItem.map(donationCard => <DonationCard key={donationCard.id} donationCard={donationCard}></DonationCard>)
+                       isShowAll ?  donationsItem?.map(donationCard => <DonationCard key={donationCard.id} donationCard={donationCard}></DonationCard>)
+                       :
+                       donationsItem?.slice(0,4).map(donationCard => <DonationCard key={donationCard.id} donationCard={donationCard}></DonationCard>)
                     }
                 </div>
+
                 {donationsItem.length >= 4 && 
                  <div className="flex justify-center mt-4">
-                    <button className="bg-[#009444] text-white capitalize p-4 rounded-lg font-medium">See All</button>
-                
-
+                    <button onClick={()=>setIsShowAll(!isShowAll)} className="bg-[#009444] text-white capitalize p-4 rounded-lg font-medium">
+                        {isShowAll? "See Less":"See All"}
+                    </button>
                 </div>}
 
             </div>
